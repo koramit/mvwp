12b79b6e58cd35f7d2ffb144af3a702385b1143f
koramit <koramit.p@gmail.com>
2023-06-22 21:52:51 +0700

    init code

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::post('/mv-wp', function () {
+    if (request()->bearerToken() !== config('app.move_wordplease_token')) {
+        abort(403, 'Unauthorized');
+    }
+
+    $file = request()->file('file');
+
+    return [
+        'key' => \Illuminate\Support\Facades\Storage::disk('s3')->putFileAs('u', $file, $file->getClientOriginalName())
+    ];
+});
